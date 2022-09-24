7f0839151dc2943437cc955a3ed5cc40caa48e49
gabriosilva <gabrio.lina@hotmail.com>
2022-09-24 16:50:18 -0300

    feat(component): add informativeFooter

---
+++ src/components/InformativeFooter.tsx
@@ -0,0 +1,33 @@
+import { IonFooter, IonHeader } from "@ionic/react";
+import { ReactElement } from "react";
+import "../styles/components/InformativeFooter.css";
+
+interface InformativeFooterProps {
+  footerTitle: string;
+  footerIcon: ReactElement;
+  footerText: string;
+  footerActionElement: ReactElement;
+}
+
+const InformativeFooter: React.FC<InformativeFooterProps> = ({
+  footerTitle,
+  footerIcon,
+  footerText,
+  footerActionElement,
+}) => {
+  return (
+    <>
+      <IonHeader className="block-header">
+        <h5>{footerTitle}</h5>
+        {footerIcon}
+      </IonHeader>
+
+      <div className="block-content">
+        <h6 className="block-text">{footerText}</h6>
+        <IonFooter className="block-footer">{footerActionElement}</IonFooter>
+      </div>
+    </>
+  );
+};
+
+export default InformativeFooter;
--- src/pages/accessOpenInvestment.tsx
+++ src/pages/accessOpenInvestment.tsx
@@ -1,5 +1,40 @@
-const AccessOpenInvestment: React.FC = () => {
-  return (<></>)
-}
+import { IonButton } from "@ionic/react";
+import InformativeFooter from "../components/InformativeFooter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleExclamation,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-export default AccessOpenInvestment
+const InfoIcon: React.FC = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faCircleExclamation}
+      style={{ color: "#FA6450", fontSize: "1.4em" }}
+    />
+  );
+};
+
+const NextPageIcon: React.FC = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faArrowRight}
+      style={{ color: "#4C535D", fontSize: "1.4em" }}
+    />
+  );
+};
+
+const AccessOpenInvestment: React.FC = () => {
+  return (
+    <>
+      <InformativeFooter
+        footerTitle="Tributos Pendentes"
+        footerText="Facilite a forma como você declara os seus impostos com o Open Investment."
+        footerIcon={<InfoIcon />}
+        footerActionElement={<NextPageIcon />}
+      />
+    </>
+  );
+};
+
+export default AccessOpenInvestment;
