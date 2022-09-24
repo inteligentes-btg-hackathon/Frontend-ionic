8c45720a73d9767f559d511b921ac4e61e5c08ba
gabriosilva <gabrio.lina@hotmail.com>
2022-09-24 13:50:53 -0300

    First Commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import MessageListItem from "../components/MessageListItem";
+import { useState } from "react";
+import { Message, getMessages } from "../data/messages";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  useIonViewWillEnter,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
+import "./Home.css";
+
+const Home: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useIonViewWillEnter(() => {
+    const msgs = getMessages();
+    setMessages(msgs);
+  });
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete();
+    }, 3000);
+  };
+
+  return (
+    <IonPage id="home-page">
+      <IonHeader class="header">
+        <img height={50} className={"title-img"} src="assets/logoBtg.png" />
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="10">
+              <div className="content-block">
+                <h3>
+                  Que bom te ver por
+                  <br />
+                  aqui, Henrique!
+                </h3>
+                <IonButton className="login-button">Entrar</IonButton>
+                <h5>Entrar como uma nova conta {">"}</h5>
+                <h5>Iniciar Cadastro {">"}</h5>
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
